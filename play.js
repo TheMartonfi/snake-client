99d254f20d836e2bd8d29199b1bacadbc742b327
TheMartonfi <carlmartonfi@hotmail.com>
2020-08-26 21:22:07 +0000

    Refactored function into own file

--- play.js
+++ play.js
@@ -1,26 +1,9 @@
 const { connect } = require('./client');
+const { setupInput } = require('./input');
 
-const setupInput = () => {
-  
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding('utf8');
-  stdin.resume();
-
-  const handleUserInput = () => {
-    stdin.on('data', (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      } 
-    });
-  };
-  
-  handleUserInput();
-  return stdin;
-
-};
 
 console.log('Connecting...');
-connect();
-setupInput();
 
+connect();
+
+setupInput();
