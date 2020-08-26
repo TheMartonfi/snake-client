2ce2dec0e9281da4513c4a9fd226c948c13fea1f
TheMartonfi <carlmartonfi@hotmail.com>
2020-08-26 21:18:03 +0000

    Added keyboard inputs and ctrl+c close feature

--- play.js
+++ play.js
@@ -1,4 +1,26 @@
 const { connect } = require('./client');
 
+const setupInput = () => {
+  
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+
+  const handleUserInput = () => {
+    stdin.on('data', (key) => {
+      if (key === '\u0003') {
+        process.exit();
+      } 
+    });
+  };
+  
+  handleUserInput();
+  return stdin;
+
+};
+
 console.log('Connecting...');
 connect();
+setupInput();
+
