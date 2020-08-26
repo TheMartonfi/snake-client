60e106edd0febb084f18a3cc0545b435a980801e
TheMartonfi <carlmartonfi@hotmail.com>
2020-08-26 19:17:16 +0000

    Can connect to server and receive data from server

---
+++ play.js
@@ -0,0 +1,20 @@
+const net = require('net');
+
+const connect = function() {
+
+  const conn = net.createConnection({
+    host: '192.168.0.139',
+    port: 50542
+  });
+
+  conn.setEncoding('utf8');
+
+  conn.on('data', (data) => {
+    console.log('Data: ', data);
+  });
+
+  return conn;
+};
+
+console.log('Connecting...');
+connect();
