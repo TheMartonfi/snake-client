27bd362065b4277cc19d3c1b160aa97604f1e88e
TheMartonfi <carlmartonfi@hotmail.com>
2020-08-26 21:28:58 +0000

    Inputs now sends data to the server

--- input.js
+++ input.js
@@ -1,5 +1,9 @@
-const setupInput = () => {
+let connection;
+
+const setupInput = (conn) => {
   
+  connection = conn;
+
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -7,9 +11,20 @@ const setupInput = () => {
 
   const handleUserInput = () => {
     stdin.on('data', (key) => {
+
       if (key === '\u0003') {
+        conn.write('The application is now closing.');
         process.exit();
-      } 
+      } else if (key === 'w') {
+        conn.write('Move: up');
+      } else if (key === 'a') {
+        conn.write('Move: left');
+      } else if (key === 's') {
+        conn.write('Move: down');
+      } else if (key === 'd') {
+        conn.write('Move: right');
+      }
+
     });
   };
   
